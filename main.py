55dbba5b45702639e0cec69dc10ac874675a5018
Karthik Kannan <karthikk.314@gmail.com>
2017-03-12 22:00:51 +0530

    Use Twython for the whole thing. Write to a text file.  TODO: Format the text.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
+from twython import Twython
 from flask import Flask, render_template,request,redirect
-from twitter import *
 import requests
 from requests_oauthlib import OAuth1Session
+import time
+
 
 import json
 app = Flask(__name__, static_folder='templates')
@@ -15,15 +17,33 @@ consumer_secret = "ilmcb9Rr4RIGa0zRkaCti3OUHDZ6JVLJ9nm9k7AzPbWHb1kBD1"
 
 tweets_text = []
 
-twitter = OAuth1Session(consumer_key,client_secret=consumer_secret, resource_owner_key=token, resource_owner_secret=token_secret)
+#twitter = OAuth1Session(consumer_key,client_secret=consumer_secret, resource_owner_key=token, resource_owner_secret=token_secret)
 status_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="
 
-def get_tweets_text(tweets_json):
-## Get the text from the tweets and write it to a file. ##
-	for i in range(len(tweets_json)):
-		tweets_text.append(tweets_json[i]['text'])
-		print(tweets_text)
-		print(len(tweets_text))
+twitter = Twython(consumer_key,consumer_secret,token,token_secret)
+lis = []
+tweet_text = []
+thefile = open('pred.txt', 'w')
+
+# def get_tweets_text(tweets_json):
+# ## Get the text from the tweets and write it to a file. ##
+# 	for i in range(len(tweets_json)):
+# 		tweets_text.append(tweets_json[i]['text'])
+# 		print(tweets_text)
+# 		print(len(tweets_text))
+
+def get_tweets_text2(handle):
+	user_timeline = twitter.get_user_timeline(screen_name=handle,count=1)
+	lis.append(user_timeline[0]['id'])
+	for i in range(0, 16):
+		user_timeline = twitter.get_user_timeline(screen_name=handle, count=200, include_rts=False, max_id=lis[-1])
+		for tweet in user_timeline:
+			#print(tweet['text'])
+			tweet_text.append(tweet['text'])
+			lis.append(tweet['id'])
+	for tweet in tweet_text:
+		thefile.write("%s\n" %tweet)
+	print("done writing tweets to the file!")
 
 
 @app.route('/') #index.
@@ -33,12 +53,12 @@ def index():
 @app.route('/result',methods=['POST'])
 def result():
 	handle = request.form['twitter-handle']
-	# tl = t.statuses.user_timeline(screen_name=handle)
-	tweets = twitter.get(status_url+handle+'&count=200&include_rts=false&exclude_replies=true')
-	tweets_json = tweets.json()
-	get_tweets_text(tweets_json)
-	#print(tweets_json[0]['text'])
+	del lis[:]
+	del tweet_text[:]
+	get_tweets_text2(handle)
 	return redirect('/')
+
+
 if __name__ == '__main__':
 	app.run()
 
